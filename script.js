93915ac17251b036aa1e67dd83ececeb73028c22
Daniel Galndo <d.galindo@uniandes.edu.co>
2020-09-22 09:11:56 -0500

    Avances

---
+++ script.js
@@ -0,0 +1,24 @@
+const data = "https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json";
+const navLinks= document.getElementsByTagName("a");
+const products = document.getElementById("products");
+let jsonData = []
+for(let i=0; i<navLinks.length;i++){
+    navLinks[i].addEventListener("click", () => renderCards(navLinks[i].id));
+}
+
+fetch(data)
+.then((response) => response.json())
+.then((data) => {
+    jsonData = data;
+    //console.log(jsonData);
+});
+
+const renderCards = (productName) => {
+    let listaProds = []
+    jsonData.forEach((item) => {
+        if(item.name === productName){
+            listaProds = item.products;
+        }
+    })
+    //console.log(productName);
+}
